49c670df096d7048877b8d711b667406eaa7a42c
xLvkas <lukasz.chmielewski@student.wsb.edu.pl>
2023-05-25 16:10:48 +0200

    third commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $faker = Faker::create();
+
+        for ($i = 0;$i<200;$i++){
+            \App\People::create([
+                'name'=>$faker->name,
+                'email'=>$faker->email,
+                'phone'=>$faker->phoneNumber,
+                'gender'=>$faker->text
+            ]);
+        }
     }
 }
